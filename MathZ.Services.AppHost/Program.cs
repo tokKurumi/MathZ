462923090b185df27277d099ffec15d63fcebb75
Oleg Yudashkin <tkurumi901@gmail.com>
2024-01-06 03:40:36 +0300

    removed not necessary package references in DistributionAPI, simplified AppHost Program file

--- MathZ.Services.AppHost/Program.cs
+++ MathZ.Services.AppHost/Program.cs
@@ -1,43 +1,35 @@
-namespace MathZ.Services.AppHost;
+var builder = DistributedApplication.CreateBuilder(args);
 
-public class Program
-{
-    private static void Main(string[] args)
-    {
-        var builder = DistributedApplication.CreateBuilder(args);
+string jwtAudience = @"mathz-client";
+string jwtIssuer = "mathz-auth-api";
+string jwtSecret = @"642477c9-7840-4563-b0a2-02c677eb2db0";
 
-        string jwtAudience = @"mathz-client";
-        string jwtIssuer = "mathz-auth-api";
-        string jwtSecret = @"642477c9-7840-4563-b0a2-02c677eb2db0";
+var usersDb = builder.AddPostgresContainer("users")
+    .WithVolumeMount("mathz.databases.users", "/var/lib/postgresql/data/", VolumeMountType.Named)
+    .AddDatabase("usersDb");
 
-        var usersDb = builder.AddPostgresContainer("users")
-            .WithVolumeMount("mathz.databases.users", "/var/lib/postgresql/data/", VolumeMountType.Named)
-            .AddDatabase("usersDb");
+var authApi = builder.AddProject<Projects.MathZ_Services_AuthAPI>("mathz.services.authapi")
+    .WithReference(usersDb)
+    .WithEnvironment("JwtOptions:Audience", jwtAudience)
+    .WithEnvironment("JwtOptions:Issuer", jwtIssuer)
+    .WithEnvironment("JwtOptions:Secret", jwtSecret);
 
-        var distributionAPI = builder.AddProject<Projects.MathZ_Services_DistributionAPI>("mathz.services.distributionapi")
-            .WithEnvironment("JwtOptions:Audience", jwtAudience)
-            .WithEnvironment("JwtOptions:Issuer", jwtIssuer)
-            .WithEnvironment("JwtOptions:Secret", jwtSecret);
+var distributionAPI = builder.AddProject<Projects.MathZ_Services_DistributionAPI>("mathz.services.distributionapi")
+    .WithEnvironment("JwtOptions:Audience", jwtAudience)
+    .WithEnvironment("JwtOptions:Issuer", jwtIssuer)
+    .WithEnvironment("JwtOptions:Secret", jwtSecret);
 
-        var authApi = builder.AddProject<Projects.MathZ_Services_AuthAPI>("mathz.services.authapi")
-            .WithReference(usersDb)
-            .WithEnvironment("JwtOptions:Audience", jwtAudience)
-            .WithEnvironment("JwtOptions:Issuer", jwtIssuer)
-            .WithEnvironment("JwtOptions:Secret", jwtSecret);
+var userApi = builder.AddProject<Projects.MathZ_Services_UserAPI>("mathz.services.userapi")
+    .WithReference(usersDb)
+    .WithEnvironment("JwtOptions:Audience", jwtAudience)
+    .WithEnvironment("JwtOptions:Issuer", jwtIssuer)
+    .WithEnvironment("JwtOptions:Secret", jwtSecret);
 
-        var userApi = builder.AddProject<Projects.MathZ_Services_UserAPI>("mathz.services.userapi")
-            .WithReference(usersDb)
-            .WithEnvironment("JwtOptions:Audience", jwtAudience)
-            .WithEnvironment("JwtOptions:Issuer", jwtIssuer)
-            .WithEnvironment("JwtOptions:Secret", jwtSecret);
+var adminApi = builder.AddProject<Projects.MathZ_Services_AdminAPI>("mathz.services.adminapi")
+    .WithReference(usersDb)
+    .WithReference(authApi)
+    .WithEnvironment("JwtOptions:Audience", jwtAudience)
+    .WithEnvironment("JwtOptions:Issuer", jwtIssuer)
+    .WithEnvironment("JwtOptions:Secret", jwtSecret);
 
-        var adminApi = builder.AddProject<Projects.MathZ_Services_AdminAPI>("mathz.services.adminapi")
-            .WithReference(usersDb)
-            .WithReference(authApi)
-            .WithEnvironment("JwtOptions:Audience", jwtAudience)
-            .WithEnvironment("JwtOptions:Issuer", jwtIssuer)
-            .WithEnvironment("JwtOptions:Secret", jwtSecret);
-
-        builder.Build().Run();
-    }
-}
+builder.Build().Run();
