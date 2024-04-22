8eea47da102edc5afa8bb9af87cf833e3637d74d
Oleg Yudashkin <tkurumi901@gmail.com>
2024-04-22 05:53:08 +0300

    The most significant changes involve the removal of JWT and SMTP configurations from the `Program.cs` file and the addition of two new methods to the `ResourceBuilderExtensions` class. These changes aim to improve code maintainability and reduce code duplication.
    1. The JWT and SMTP configurations were removed from the `Program.cs` file. Instead, the code now passes the configuration section names to the `WithJwt` and `WithSmtp` methods. This change was implemented in the `identityApi`, `emailApi`, and `forumApi` builder configurations.
    
    2. Two new methods, `WithJwt` and `WithSmtp`, were added to the `ResourceBuilderExtensions` class in the `ResourceBuilderExtensions.cs` file. These methods take a configuration section name, retrieve the relevant configuration values, and then call the existing `WithJwt` and `WithSmtp` methods with these values. This encapsulates the logic for retrieving configuration values within these methods, thereby reducing code duplication and improving maintainability.

--- src/MathZ.AppHost/Program.cs
+++ src/MathZ.AppHost/Program.cs
@@ -3,20 +3,6 @@ using MathZ.Shared;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-// JWT
-var jwtOptions = builder.Configuration.GetSection("JwtOptions");
-var jwtSecret = jwtOptions["Secret"];
-var jwtAudience = jwtOptions["Audience"];
-var jwtIssuer = jwtOptions["Issuer"];
-
-// SMTP
-var smtpOptions = builder.Configuration.GetSection("SMTP");
-var smtpFrom = smtpOptions["From"];
-var smtpHost = smtpOptions["Host"];
-var smtpPort = int.Parse(smtpOptions["Port"] ?? string.Empty);
-var smtpUserName = smtpOptions["UserName"];
-var smtpPassword = builder.Configuration["SMTP:Password"];
-
 // Database passwords
 var databasePasswords = builder.Configuration.GetSection("DatabasePasswords");
 var identityDatabasePassword = databasePasswords["Identity"];
@@ -37,7 +23,7 @@ var identityDatabase = builder
 
 var identityApi = builder
     .AddProject<Projects.MathZ_Services_IdentityApi>(AspireConnections.Api.IdentityApi)
-    .WithJwt(jwtSecret, jwtAudience, jwtIssuer)
+    .WithJwt("JwtOptions")
     .WithReference(identityDatabase)
     .WithReference(messageBus);
 
@@ -50,8 +36,8 @@ var emailDatabase = builder
 
 var emailApi = builder
     .AddProject<Projects.MathZ_Services_EmailApi>(AspireConnections.Api.EmailApi)
-    .WithJwt(jwtSecret, jwtAudience, jwtIssuer)
-    .WithSmtp(smtpFrom, smtpHost, smtpPort, smtpUserName, smtpPassword)
+    .WithJwt("JwtOptions")
+    .WithSmtp("SMTP")
     .WithReference(emailDatabase)
     .WithReference(messageBus);
 
@@ -64,7 +50,7 @@ var forumDatabase = builder
 
 var forumApi = builder
     .AddProject<Projects.MathZ_Services_ForumApi>(AspireConnections.Api.ForumApi)
-    .WithJwt(jwtSecret, jwtAudience, jwtIssuer)
+    .WithJwt("JwtOptions")
     .WithReference(forumDatabase);
 
 builder.AddYarp(AspireConnections.ApiGateway)
--- src/MathZ.AppHost/ResourceBuilderExtensions.cs
+++ src/MathZ.AppHost/ResourceBuilderExtensions.cs
@@ -6,12 +6,36 @@ using MathZ.Shared.Smtp;
 
 public static class ResourceBuilderExtensions
 {
+    public static IResourceBuilder<ProjectResource> WithJwt(this IResourceBuilder<ProjectResource> resourceBuilder, string configSectionName)
+    {
+        var configuration = resourceBuilder.ApplicationBuilder.Configuration;
+
+        var jwtSecret = configuration[$"{configSectionName}:Secret"];
+        var jwtAudience = configuration[$"{configSectionName}:Audience"];
+        var jwtIssuer = configuration[$"{configSectionName}:Issuer"];
+
+        return resourceBuilder.WithJwt(jwtSecret, jwtAudience, jwtIssuer);
+    }
+
     public static IResourceBuilder<ProjectResource> WithJwt(this IResourceBuilder<ProjectResource> resourceBuilder, string? secret, string? audience, string? issuer)
     {
         return resourceBuilder
             .WithEnvironment(JwtEnvConstants.JwtSecret, secret)
             .WithEnvironment(JwtEnvConstants.JwtAudience, audience)
             .WithEnvironment(JwtEnvConstants.JwtIssuer, issuer);
+    }
+
+    public static IResourceBuilder<ProjectResource> WithSmtp(this IResourceBuilder<ProjectResource> resourceBuilder, string configSectionName)
+    {
+        var configuration = resourceBuilder.ApplicationBuilder.Configuration;
+
+        var smtpFrom = configuration[$"{configSectionName}:From"];
+        var smtpHost = configuration[$"{configSectionName}:Host"];
+        var smtpPort = int.Parse(configuration[$"{configSectionName}:Port"] ?? string.Empty);
+        var smtpUserName = configuration[$"{configSectionName}:UserName"];
+        var smtpPassword = configuration[$"{configSectionName}:Password"];
+
+        return resourceBuilder.WithSmtp(smtpFrom, smtpHost, smtpPort, smtpUserName, smtpPassword);
     }
 
     public static IResourceBuilder<ProjectResource> WithSmtp(this IResourceBuilder<ProjectResource> resourceBuilder, string? from, string? host, int port, string? userName, string? password)
